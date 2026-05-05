e577e15a8af683e9655afcc6372040bf1baec8fd
LAPTOP-UENLTB1C\Diana <coding@ddinis.ca>
2026-05-05 17:51:33 -0400

    Chaser changes

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,28 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Chaser : MonoBehaviour
+{
+    public Camera gameCamera; // create camera variable
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get mouse position
+        Vector3 currentMousePosition = Mouse.current.position.ReadValue();
+        // because currentMousePosition will be real world values, chaser will be off screen
+        // create a world mouse position to set mouse within window
+        Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
+        // set worldMousePosition to Z or else it will be at the camera Z location
+        worldMousePosition.z = 0f;
+        transform.position = worldMousePosition; // move circle to mouse position
+
+    }
+}
