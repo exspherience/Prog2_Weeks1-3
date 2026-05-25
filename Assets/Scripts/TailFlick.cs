ac7d9741ecc7aa61f01129d3a2ec30208c032062
LAPTOP-UENLTB1C\Diana <coding@ddinis.ca>
2026-05-25 19:28:01 -0400

    Implemented rotation when mouse is within distance. Using only Simple Shapes. Removed Scenes and Scripts unrelated to Assignment for navigational ease.

--- Assets/Scripts/TailFlick.cs
+++ Assets/Scripts/TailFlick.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TailFlick : MonoBehaviour
 {
+    public AnimationCurve flickCurve;
+    public float targetAngle;
+    public float flickDistanceThreshold = 1f;
+
+    public float duration;
+    public float progress = 0f;
+
+    public bool flickTail;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,41 @@ public class TailFlick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        checkDistance();
+        flick();
     }
+
+    // Check that Mouse is near tail
+    void checkDistance()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        
+        // If near tail, rotate
+        if(Vector2.Distance(transform.position, mousePos) < flickDistanceThreshold)
+        {
+            flickTail = true;
+        }
+    }
+    void flick()
+    {
+        if (flickTail)
+        {
+            progress += Time.deltaTime;
+
+            // evaluate the rotation of the tailAngle vector
+            // multiple the Evaluated curve by the targetAngle so it will rotate to that Angle
+            Vector3 tailAngle = (flickCurve.Evaluate(progress / duration) * targetAngle) * Vector3.one;
+
+            // Set eulerAngles to 0 in x and y, and tailAngle.z in z
+            // This allows for only the Z to rotate 
+            transform.eulerAngles = new Vector3(0, 0, tailAngle.z);
+
+            if (progress >= 2)
+            {
+                flickTail = false;
+                progress = 0f;
+            }
+        }
+    }
+
 }
